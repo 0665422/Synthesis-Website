c44ae31667b5fce1cc5c0bf9c976bbbaf3e16f0e
0665422 <vlazurenko22@gmail.com>
2019-12-20 10:37:55 -0500

    Timeline iterrative onesided framework

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // JavaScript source code
 
 // Sets width of timeline line
-var lineWidth = $(".timeline-line").width();
-var circleWidth = $(".circle svg").width();
-var dateWidth = $(".date").width();
+
+
 
 function keepTimelineCentered() {
+    var lineWidth = $(".timeline-line").width();
     var lineMargin = ($(window).width() - lineWidth) / 2;
     $(".timeline-line").css("margin-right", lineMargin + "px");
     $(".timeline-line").css("margin-left", lineMargin + "px");
@@ -13,14 +13,32 @@ function keepTimelineCentered() {
 
 // aligns the event
 function eventPosition() {
-    var eventMargin = ($(window).width() / 2) - circleWidth / 2 - dateWidth - 20;
-    //align to the side
-    $(".event").css("margin-right", eventMargin + "px");
 
-    $(".text").css("margin-right", (circleWidth + dateWidth + 40) + "px");
+    $('#event').each(function () {
+        var circleWidth = $("#event #circle svg").width();
+        var dateWidth = $("#event #date").width();
+        var textWidth = $("#event #text").width();
 
-    $(".circle").css("right", dateWidth + 20 + "px");
+        var eventWidth = $(window).width() - ($(window).width() / 2 - circleWidth / 2 - dateWidth);
+
+        //media active variable width
+        var mediaPadding = 20;
+        var mediaWidth = eventWidth - dateWidth - circleWidth - textWidth;
+        $("#event #media").css("width", mediaWidth + "px");
+        //creates whitespace on sides
+        var imgWidth = mediaWidth - 2 * mediaPadding;
+        $("#event #media img").css("width", imgWidth + "px");
+
+        var yearFontSize = circleWidth - 8;
+        $("#event #date h3").css("font-size", yearFontSize + "px");
+        var titleFontSize = yearFontSize + 8;
+        $("#event #text h3").css("font-size", titleFontSize + "px");   
+    });
+    
 }
+
+
+    
 
 $("document").ready(function () {
 
@@ -32,3 +50,4 @@ $("document").ready(function () {
         eventPosition();
     });
 });
+
