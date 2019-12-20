e2ddbd96c0f5adab8a532afc54e96747228de3f6
0665422 <vlazurenko22@gmail.com>
2019-12-19 22:46:05 -0500

    Timeline base
    Started the framework of the timeline

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // JavaScript source code
 
 // Sets width of timeline line
-var lineWidth = $(".timeline-line").width()
+var lineWidth = $(".timeline-line").width();
+var circleWidth = $(".circle svg").width();
+var dateWidth = $(".date").width();
 
 function keepTimelineCentered() {
     var lineMargin = ($(window).width() - lineWidth) / 2;
@@ -9,11 +11,24 @@ function keepTimelineCentered() {
     $(".timeline-line").css("margin-left", lineMargin + "px");
 }
 
+// aligns the event
+function eventPosition() {
+    var eventMargin = ($(window).width() / 2) - circleWidth / 2 - dateWidth - 20;
+    //align to the side
+    $(".event").css("margin-right", eventMargin + "px");
+
+    $(".text").css("margin-right", (circleWidth + dateWidth + 40) + "px");
+
+    $(".circle").css("right", dateWidth + 20 + "px");
+}
+
 $("document").ready(function () {
 
     keepTimelineCentered();
+    eventPosition();
 
     $(window).resize(function () {
         keepTimelineCentered();
+        eventPosition();
     });
 });
